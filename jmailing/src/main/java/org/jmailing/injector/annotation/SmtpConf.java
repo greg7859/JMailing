3994a4f1027b01a10dd838d0b1c0dadf0710fe95
Gregory Cochon <gregory.cochon@gmail.com>
2014-05-01 21:50:04 +0200

    Use qualifier annotation

--- jmailing/src/main/java/org/jmailing/injector/annotation/SmtpConf.java
+++ jmailing/src/main/java/org/jmailing/injector/annotation/SmtpConf.java
@@ -1,12 +1,14 @@
 package org.jmailing.injector.annotation;
 
-import com.google.inject.BindingAnnotation;
-import java.lang.annotation.Target;
-import java.lang.annotation.Retention;
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
-import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-@BindingAnnotation @Target({ FIELD, PARAMETER, METHOD }) @Retention(RUNTIME)
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.inject.Qualifier;
+
+@Qualifier @Target({ FIELD, PARAMETER, METHOD }) @Retention(RUNTIME)
 public @interface SmtpConf {}
