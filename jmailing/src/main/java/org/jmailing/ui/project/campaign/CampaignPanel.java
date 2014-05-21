58dc088101ae69f5ac50c13c1e1ef6f30f97811c
Gregory Cochon <gregory.cochon@gmail.com>
2014-05-21 19:55:37 +0200

    Display the text in the progress bar. Add help text to prepare the mailing

--- jmailing/src/main/java/org/jmailing/ui/project/campaign/CampaignPanel.java
+++ jmailing/src/main/java/org/jmailing/ui/project/campaign/CampaignPanel.java
@@ -10,8 +10,10 @@ import java.util.List;
 
 import javax.inject.Inject;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JProgressBar;
 
 import org.apache.commons.lang.StringUtils;
 import org.jmailing.injector.annotation.Csv;
@@ -54,6 +56,10 @@ public class CampaignPanel extends JPanel implements FilePanelListener,
 
 	private JButton sendBtn;
 
+	private JLabel label;
+
+	private JProgressBar progressBar;
+
 	public CampaignPanel() {
 	}
 
@@ -63,9 +69,9 @@ public class CampaignPanel extends JPanel implements FilePanelListener,
 
 		GridBagLayout gridBagLayout = new GridBagLayout();
 		gridBagLayout.columnWidths = new int[] { 0, 0 };
-		gridBagLayout.rowHeights = new int[] { 0, 0, 0, 0 };
+		gridBagLayout.rowHeights = new int[] { 0, 0, 0, 0,0 };
 		gridBagLayout.columnWeights = new double[] { 1.0, Double.MIN_VALUE };
-		gridBagLayout.rowWeights = new double[] { 0.0, 0.0, 0.0, 1.0,
+		gridBagLayout.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 1.0,
 				Double.MIN_VALUE };
 		setLayout(gridBagLayout);
 
@@ -105,14 +111,24 @@ public class CampaignPanel extends JPanel implements FilePanelListener,
 		});
 		add(sendBtn, gbc_ButtonPanel);
 
+		GridBagConstraints gbc_ProgressPanel = new GridBagConstraints();
+		gbc_ProgressPanel.anchor = GridBagConstraints.CENTER;
+		gbc_ProgressPanel.fill = GridBagConstraints.HORIZONTAL;
+		gbc_ProgressPanel.insets = new Insets(0, 0, 5, 5);
+		gbc_ProgressPanel.gridx = 0;
+		gbc_ProgressPanel.gridy = 3;
+		JPanel progressPanel = initProgressPanel();
+		add(progressPanel, gbc_ProgressPanel);
+
 		GridBagConstraints gbc_Table = new GridBagConstraints();
 		gbc_Table.anchor = GridBagConstraints.CENTER;
 		gbc_Table.fill = GridBagConstraints.BOTH;
 		gbc_Table.insets = new Insets(0, 0, 5, 5);
 		gbc_Table.gridx = 0;
-		gbc_Table.gridy = 3;
+		gbc_Table.gridy = 4;
 		add(cTablePanel, gbc_Table);
 		cTablePanel.setVisible(false);
+		updateLabel();
 	}
 
 	@Override
@@ -134,18 +150,24 @@ public class CampaignPanel extends JPanel implements FilePanelListener,
 		if (StringUtils.isNotBlank(cvsFile) && StringUtils.isNotBlank(pdfFile)) {
 			sendBtn.setEnabled(true);
 		}
+		updateLabel();
+		progressBar.setValue(0);
 	}
 
 	@Override
 	public void done() {
 		changeStateButton(true);
+		label.setText( "Campaign done !");
 		JOptionPane.showMessageDialog(this, "Campaign done !");
 	}
 
 	@Override
 	public void progress(int index, int progress, boolean state) {
-		System.out.println("Progress=" + progress + " %. Send " + (index + 1)
-				+ " on " + csvData.size());
+		label.setText("Send " + (index + 1) + " on " + csvData.size());
+		progressBar.setValue(progress);
+		progressBar.setString(progress+" %");
+//		System.out.println("Progress=" + progress + " %. Send " + (index + 1)
+//				+ " on " + csvData.size());
 	}
 
 	private void changeStateButton(boolean b) {
@@ -165,6 +187,50 @@ public class CampaignPanel extends JPanel implements FilePanelListener,
 				break;
 			}
 		}
+	}
+	
+	private void updateLabel() {
+		if (StringUtils.isNotBlank(cvsFile) && StringUtils.isNotBlank(pdfFile)) {
+			sendBtn.setEnabled(true);
+			label.setText("Click on button \"Send\" to start the mailing.");
+		}
+		else if (StringUtils.isBlank(cvsFile)) {
+			label.setText("Select a CSV File.");			
+		}
+		else if (StringUtils.isBlank(pdfFile)) {
+			label.setText("Select a PDF File.");			
+		}		
+	}
+
+	private JPanel initProgressPanel() {
+		JPanel p = new JPanel();
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[] { 0, 0, 0 };
+		gridBagLayout.rowHeights = new int[] { 0, 0 };
+		gridBagLayout.columnWeights = new double[] { 0.5, 0.5, Double.MIN_VALUE };
+		gridBagLayout.rowWeights = new double[] { 1.0, Double.MIN_VALUE };
+		p.setLayout(gridBagLayout);
+
+		label = new JLabel();
+		GridBagConstraints gbc_Label = new GridBagConstraints();
+		gbc_Label.anchor = GridBagConstraints.CENTER;
+		gbc_Label.fill = GridBagConstraints.HORIZONTAL;
+		gbc_Label.insets = new Insets(0, 0, 5, 5);
+		gbc_Label.gridx = 0;
+		gbc_Label.gridy = 0;
+		p.add(label, gbc_Label);
+
+		GridBagConstraints gbc_ProgressMonitor = new GridBagConstraints();
+		gbc_ProgressMonitor.anchor = GridBagConstraints.CENTER;
+		gbc_ProgressMonitor.fill = GridBagConstraints.HORIZONTAL;
+		gbc_ProgressMonitor.insets = new Insets(0, 0, 5, 5);
+		gbc_ProgressMonitor.gridx = 1;
+		gbc_ProgressMonitor.gridy = 0;
+		progressBar = new JProgressBar();
+		progressBar.setStringPainted(true);
+		p.add(progressBar, gbc_ProgressMonitor);
+
+		return p;
 	}
 
 }
