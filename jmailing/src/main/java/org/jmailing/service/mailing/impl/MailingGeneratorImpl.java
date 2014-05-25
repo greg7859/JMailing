fd89f4bcf39e0b7103563803b67f8fee25fed7dd
Gregory Cochon <gregory.cochon@gmail.com>
2014-05-26 00:43:45 +0200

    Log event and save sent email.

--- jmailing/src/main/java/org/jmailing/service/mailing/impl/MailingGeneratorImpl.java
+++ jmailing/src/main/java/org/jmailing/service/mailing/impl/MailingGeneratorImpl.java
@@ -11,17 +11,20 @@ import javax.inject.Inject;
 import javax.swing.SwingWorker;
 
 import org.jmailing.config.ConfigHelper;
+import org.jmailing.injector.annotation.Pdf;
 import org.jmailing.injector.annotation.ProjectPath;
 import org.jmailing.injector.annotation.VariablePrefix;
 import org.jmailing.injector.annotation.VariableSuffix;
 import org.jmailing.injector.provider.AttachmentProvider;
 import org.jmailing.injector.provider.EMailProvider;
+import org.jmailing.io.email.EMailIO;
 import org.jmailing.model.email.Attachment;
 import org.jmailing.model.email.EMail;
 import org.jmailing.model.project.EmailMailingProjectPart;
 import org.jmailing.model.project.MailingProject;
 import org.jmailing.model.project.SourceVariable;
 import org.jmailing.model.source.Data;
+import org.jmailing.service.logger.FileLogger;
 import org.jmailing.service.mail.EmailService;
 import org.jmailing.service.mail.EmailServiceException;
 import org.jmailing.service.mailing.AttachmentSplitter;
@@ -29,6 +32,7 @@ import org.jmailing.service.mailing.AttachmentSplitterException;
 import org.jmailing.service.mailing.MailingGenerator;
 import org.jmailing.service.mailing.MailingGeneratorEvent;
 import org.jmailing.service.mailing.MailingGeneratorListener;
+import org.jmailing.utilities.FileNameUtilities;
 
 import com.google.common.base.Splitter;
 
@@ -44,6 +48,10 @@ public class MailingGeneratorImpl implements MailingGenerator {
 	@Inject
 	@VariableSuffix
 	private String suffix;
+
+	@Inject
+	@Pdf
+	private String pdfExtension;
 
 	@Inject
 	@ProjectPath
@@ -64,6 +72,18 @@ public class MailingGeneratorImpl implements MailingGenerator {
 	@Inject
 	private EmailService emailSvc;
 
+	@Inject
+	private FileLogger logger;
+
+	@Inject
+	private FileNameUtilities filenameUtilities;
+
+	@Inject
+	private EMailIO emailIO;
+
+	private SimpleDateFormat dateFormat = new SimpleDateFormat(
+			"yyyy-MM-dd-HH-mm-ss");
+
 	@Override
 	public void sendCampaign(final List<Data> data, final String filename) {
 		SwingWorker<Integer, MailingGeneratorEvent> worker = new SwingWorker<Integer, MailingGeneratorEvent>() {
@@ -73,33 +93,39 @@ public class MailingGeneratorImpl implements MailingGenerator {
 				int nbPage = project.getAttachmentMailingProjectPart()
 						.getNumberOfPageOfSplit();
 				try {
-					Calendar cal = Calendar.getInstance();
-					SimpleDateFormat dateFormat = new SimpleDateFormat(
-							"yyyy-MM-dd-HH-mm-ss");
-					String dateStr = dateFormat.format(cal.getTime());
+					String dateStr = dateFormat.format(Calendar.getInstance()
+							.getTime());
 					String pathProject = path + File.separator
 							+ project.getName() + File.separator + dateStr;
 					ConfigHelper.checkDirectory(pathProject);
+					logger.start(pathProject);
+					logger.log("Begin campaign");
+					// Split Attachment
+					logger.log("Start Split");
 					attSplitter.split(filename, pathProject, SPLITTED_FILENAME,
 							nbPage);
-					int index = 0;
+					logger.log("Start Split done");
 					String format = project.getAttachmentMailingProjectPart()
 							.getFilenameFormat();
 					int sleepTime = 3600000 / project
 							.getMailingConfigurationPart()
 							.getNumberMailPerHour();
 
-					/* Transmet la nouvelle progression. */
+					/* publish the progress. */
 					int progress = 20;
 					publish(new MailingGeneratorEvent(EVENT_SPLIT, progress, 0));
 					int size = data.size();
 					int step = 80 / size;
 
+					int index = 0;
 					for (Data itemData : data) {
+						logger.log("Prepare email no" + (index + 1));
 						EMail email = generateEMail(itemData);
 						Attachment attachment = generateAttachment(index,
 								itemData, format, pathProject);
 						email.addAttachment(attachment);
+						logger.log("Email no" + (index + 1) + " is sent");
+						emailIO.save(pathProject, email, index + 1);
 						try {
 							progress += step;
 							emailSvc.sendHtmlMessage(email);
@@ -109,6 +135,8 @@ public class MailingGeneratorImpl implements MailingGenerator {
 						} catch (EmailServiceException e1) {
 							publish(new MailingGeneratorEvent(
 									EVENT_EMAIL_UNSENT, progress, index));
+							logger.log("Email no" + (index + 1) + " isn't sent");
+							logger.log(e1.getMessage());
 						}
 						index++;
 						if (index != size)
@@ -123,6 +151,8 @@ public class MailingGeneratorImpl implements MailingGenerator {
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
+				logger.log("Mailing done");
+				logger.stop();
 				return new Integer(1);
 			}
 
@@ -149,6 +179,12 @@ public class MailingGeneratorImpl implements MailingGenerator {
 
 		};
 		worker.execute();
+	}
+
+	@Override
+	public String getAttachmentPath(int i) {
+		// Not yet implemented
+		return null;
 	}
 
 	@Override
@@ -187,12 +223,6 @@ public class MailingGeneratorImpl implements MailingGenerator {
 		return email;
 	}
 
-	@Override
-	public String getAttachmentPath(int i) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 	private Attachment generateAttachment(int index, Data data,
 			String filenameFormat, String pathProject) {
 		SourceVariable[] variables = project.getSourceMailingProjectPart()
@@ -201,8 +231,8 @@ public class MailingGeneratorImpl implements MailingGenerator {
 
 		Attachment attachment = attachmentProvider.get();
 		attachment.setName(filename);
-		String file = pathProject + File.separator + SPLITTED_FILENAME
-				+ String.format("%03d", index + 1) + ".pdf";
+		String file = filenameUtilities.build(pathProject, SPLITTED_FILENAME,
+				index + 1, pdfExtension);
 		attachment.setPath(file);
 
 		return attachment;
