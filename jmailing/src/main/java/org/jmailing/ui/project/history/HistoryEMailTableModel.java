5b56aa1034f1df0331338bfedda377f900fe0746
Gregory Cochon <gregory.cochon@gmail.com>
2014-07-23 16:35:00 +0200

    Display attached document (from history)

--- jmailing/src/main/java/org/jmailing/ui/project/history/HistoryEMailTableModel.java
+++ jmailing/src/main/java/org/jmailing/ui/project/history/HistoryEMailTableModel.java
@@ -1,7 +1,10 @@
 package org.jmailing.ui.project.history;
 
+import java.awt.Desktop;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 import javax.swing.JButton;
@@ -9,6 +12,7 @@ import javax.swing.JOptionPane;
 import javax.swing.table.AbstractTableModel;
 
 import org.apache.commons.lang.StringUtils;
+import org.jmailing.model.email.Attachment;
 import org.jmailing.model.email.EMail;
 import org.jmailing.ui.common.dialog.HtmlDialog;
 
@@ -21,13 +25,14 @@ public class HistoryEMailTableModel extends AbstractTableModel {
 	private List<EMail> emails = null;
 
 	final static private String[] HEADERS = { "", "Subject", "To", "Cc", "Bcc",
-			"Body" };
+			"Body", "PDF" };
 	final static private int INDEX = 0;
 	final static private int SUBJECT = 1;
 	final static private int TO = 2;
 	final static private int CC = 3;
 	final static private int BCC = 4;
 	final static private int BODY = 5;
+	final static private int PDF = 6;
 
 	public void setEMail(List<EMail> emails) {
 		this.emails = emails;
@@ -63,10 +68,37 @@ public class HistoryEMailTableModel extends AbstractTableModel {
 			return StringUtils.join(emails.get(rowIndex).getBcc(), ",");
 		} else if (colIndex == BODY) {
 
-			final JButton button = new JButton("See email");
+			final JButton button = new JButton("Show email");
 			button.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent arg0) {
-					new HtmlDialog(emails.get(rowIndex).getBody()).setVisible(true);
+					new HtmlDialog(emails.get(rowIndex).getBody())
+							.setVisible(true);
+				}
+			});
+			return button;
+		} else if (colIndex == PDF) {
+			final JButton button = new JButton("Show PDF");
+			button.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					if (Desktop.isDesktopSupported()) {
+						Desktop desktop = Desktop.getDesktop();
+						Attachment[] atts = emails.get(rowIndex)
+								.getAttachments();
+						if (atts.length > 0) {
+							File file = new File(atts[0].getPath());
+							if (desktop.isSupported(Desktop.Action.OPEN)) {
+								try {
+									desktop.open(file);
+								} catch (IOException ioe) {
+									JOptionPane.showMessageDialog(JOptionPane
+											.getFrameForComponent(button), ioe
+											.getMessage(), "Error PDF opening",
+											JOptionPane.ERROR_MESSAGE);
+									ioe.printStackTrace();
+								}
+							}
+						}
+					}
 				}
 			});
 			return button;
@@ -81,7 +113,7 @@ public class HistoryEMailTableModel extends AbstractTableModel {
 
 	@Override
 	public Class<?> getColumnClass(int columnIndex) {
-		if (columnIndex == BODY)
+		if (columnIndex == BODY || columnIndex == PDF)
 			return JButton.class;
 		else
 			return String.class;
